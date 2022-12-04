47a3aab80d5138ced555d7d83a9ea974a17cf4ba
evgeny <oxothnk111@gmail.com>
2022-12-04 14:33:04 +0000

    final commit(hope so)

--- servicedb/main.py
+++ servicedb/main.py
@@ -1,76 +1,15 @@
-import os
 import asyncio
+import os
+import json
 
-import databases
 import aio_pika
-import aiohttp
-import sqlalchemy
-from fastapi import FastAPI
-from pydantic import BaseModel
+import asyncio
+import asyncpg
+
+TABLE_NAME = 'appeals'
 
 
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
-DB_USER = os.environ.get('DB_USER')
-DB_PASSWORD = os.environ.get('DB_PASSWORD')
-DB_NAME = os.environ.get('DB_NAME')
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# SQLAlchemy specific code, as with any other app
-# DATABASE_URL = "sqlite:///./test.db"
-
-database = databases.Database(DATABASE_URL)
-
-metadata = sqlalchemy.MetaData()
-
-appeals = sqlalchemy.Table(
-    "appeals",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("firstname", sqlalchemy.String),
-    sqlalchemy.Column("surname", sqlalchemy.String),
-    sqlalchemy.Column("middlename", sqlalchemy.String),
-    sqlalchemy.Column("phone_number", sqlalchemy.Integer),
-    sqlalchemy.Column("text_message", sqlalchemy.String),
-)
-
-
-engine = sqlalchemy.create_engine(
-    DATABASE_URL,
-)
-metadata.create_all(engine)
-
-class Appeal(BaseModel):
-    firstname: str
-    surname: str
-    middlename: str
-    phone_number: int
-    text_message: str
-
-
-app = FastAPI()
-
-
-@app.on_event("startup")
-async def startup():
-    await database.connect()
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
-
-@app.post("/appeal/", response_model=Appeal)
-async def create_note(appeal: Appeal):
-    query = appeals.insert().values(
-        firstname=appeal.firstname,
-        surname=appeal.surname,
-        middlename=appeal.middlename,
-        phone_number=appeal.phone_number,
-        text_message=appeal.text_message)
-    last_record_id = await database.execute(query)
-    return {**appeal.dict(), "id": last_record_id}
-
-async def pika_consume(amqp_connection) -> None:
+async def pika_consume(amqp_connection, db_connection) -> None:
 
     queue_name = "form_data"
 
@@ -84,9 +23,23 @@ async def pika_consume(amqp_connection) -> None:
         async with queue.iterator() as queue_iter:
             async for message in queue_iter:
                 async with message.process():
-                    async with aiohttp.ClientSession() as session:
-                        await session.post('/appeal/', data=message.body)
+                    data = json.loads(message.body)
+                    data = json.loads(data) # for some reason json decode as string on first call
+                    await db_connection.execute('''
+                        INSERT INTO appeals VALUES (DEFAULT, $1, $2, $3, $4, $5);
+                        ''', *data.values())
 
+
+async def ensure_db_table(db_connection) -> None:
+    await db_connection.execute('''
+            CREATE TABLE IF NOT EXISTS appeals (
+                id SERIAL NOT NULL PRIMARY KEY,
+                firstname text,
+                surname text,
+                middlename text,
+                phone_number text,
+                text_message text);
+            ''')
 
 async def main():
     amqp_host = os.environ.get('AMQP_HOST')
@@ -94,14 +47,28 @@ async def main():
     amqp_user = os.environ.get('AMQP_USER')
     amqp_password = os.environ.get('AMQP_PASSWORD')
 
-    amqp_connection = await aio_pika.connect_robust(
-                                host=amqp_host,
-                                port=amqp_port,
-                                login=amqp_user,
-                                password=amqp_password)
+    db_host = os.environ.get('DB_HOST')
+    db_port = os.environ.get('DB_PORT')
+    db_user = os.environ.get('DB_USER')
+    db_password = os.environ.get('DB_PASSWORD')
+    db_name = os.environ.get('DB_NAME')
 
-    await pika_consume(amqp_connection)
-
+    try:
+        amqp_connection = await aio_pika.connect_robust(
+                                    host=amqp_host,
+                                    port=amqp_port,
+                                    login=amqp_user,
+                                    password=amqp_password)
+        db_connection = await asyncpg.connect(host=db_host,
+                                port=db_port,
+                                user=db_user,
+                                password=db_password,
+                                database=db_name)
+        await ensure_db_table(db_connection)
+        await pika_consume(amqp_connection, db_connection)
+    finally:
+        await amqp_connection.close()
+        await db_connection.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
